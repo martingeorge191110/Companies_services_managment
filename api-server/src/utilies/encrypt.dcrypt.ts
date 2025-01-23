0fa70d645fce1c40b5ddc68662524d886625c4ea
Martin George <martingeorge191110@gmail.com>
2025-01-23 14:10:44 +0200

    Encryption File Adjustments

--- api-server/src/utilies/encrypt.dcrypt.ts
+++ api-server/src/utilies/encrypt.dcrypt.ts
@@ -1,17 +1,19 @@
 #!/usr/bin/env ts-node
-
 import CryptoJS from 'crypto-js'
 import dotenv from 'dotenv'
+
 dotenv.config()
 
 
+const secretKey = process.env.CRYPTO_SECRET_KEY as string
+
 export const encrypting = (text: string) => {
-   const encrypted = CryptoJS.AES.encrypt(text, process.env.CRYPTO_SECRET_KEY || "").toString();
-   return encrypted;
+      const encrypted = CryptoJS.AES.encrypt(text, secretKey).toString();
+      return (encrypted);
 }
 
 export const decrypting = (encryptedText: string ) => {
-   const bytes = CryptoJS.AES.decrypt(encryptedText, process.env.CRYPTO_SECRET_KEY || "");
-   const decrypted = bytes.toString(CryptoJS.enc.Utf8);
-   return decrypted;
+      const bytes = CryptoJS.AES.decrypt(encryptedText, secretKey);
+      const decrypted = bytes.toString(CryptoJS.enc.Utf8);
+      return (decrypted);
 }
